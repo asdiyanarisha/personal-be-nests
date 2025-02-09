a2759f419a9e938288f1807b705e4e0222766ce8
risha.asdiyana <asdiyanarisha@gmail.com>
2025-02-09 07:53:24 +0700

    push upload file

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -4,6 +4,7 @@ import { Response } from 'express';
 import { CreatePostBlog } from '../dtos/blog';
 import { BuildResponseUtil } from '../util';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('api/blog')
 export class BlogController {
@@ -13,7 +14,26 @@ export class BlogController {
   ) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/',
+        filename: function (req, file, cb) {
+          const originalNameSplit = file.originalname.split('.');
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(
+            null,
+            file.fieldname +
+              '-' +
+              uniqueSuffix +
+              '.' +
+              originalNameSplit[originalNameSplit.length - 1],
+          );
+        },
+      }),
+    }),
+  )
   async createPostBlog(
     @Body() req: CreatePostBlog,
     @UploadedFile() file: Express.Multer.File,
